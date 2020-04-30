0bd9ca3455d1182c641ef09dda4e3de76ec5c99c
Ayumi Shirai <sh.ay20200501@gmail.com>
2020-04-30 15:55:41 -0500

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ayumi Shirai on 2020/04/30.
+//  Copyright © 2020 ayumi.shirai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
